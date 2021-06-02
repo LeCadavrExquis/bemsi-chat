cf4fae0f55c6c871bc9a8393dcad6bd7cf2d8d7c
Paweł Drewek <p.drewek@stud.elka.pw.edu.pl>
2021-06-02 17:58:49 +0200

    Initial commit

---
+++ src/main/java/com/example/bemsichat/BemsiChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.bemsichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemsiChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BemsiChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bemsichat/BemsiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bemsichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemsiChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
