fa39cbbce06fae60a49e0736a6e9ccd701feef57
Paweł Drewek <p.drewek@stud.elka.pw.edu.pl>
2021-06-11 17:39:50 +0200

    Simple chat and README.md added

--- web/src/main/java/com/example/bemsichat/BemsiChatApplication.java
+++ web/src/main/java/com/example/bemsichat/BemsiChatApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BemsiChatApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BemsiChatApplication.class, args);
     }
-
 }
