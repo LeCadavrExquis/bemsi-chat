b1de5d62803f8e5f464ab9523dc65779d1db9958
Filip Żarnowiec <filipzarnowiec@gmail.com>
2021-06-14 05:16:24 +0200

    symetric key encoding/decoding

--- client/src/main/kotlin/app.kt
+++ client/src/main/kotlin/app.kt
@@ -10,14 +10,15 @@ import styled.styledH1
 import ui.chat
 import ui.friendsList
 import ui.logIn
-import kotlin.math.abs
-import kotlin.random.Random
 
 external interface AppState : RState {
     var user: User
     var messages: List<Message>
     var friend: Pair<String, String>
     var stomp: dynamic
+    var symmetricKey: dynamic
+    var asymKeyPair: dynamic
+    var sharedKey: dynamic
 }
 
 class App : RComponent<RProps, AppState>() {
@@ -42,7 +43,7 @@ class App : RComponent<RProps, AppState>() {
 
     override fun RBuilder.render() {
         styledH1 {
-            +"Bemsi ui.Chat App"
+            +"Bemsi Chat App"
             css {
                 fontSize = 4.em
                 fontWeight = FontWeight.bold
@@ -57,6 +58,7 @@ class App : RComponent<RProps, AppState>() {
                 }
                 logIn {
                     onLogIn = this@App::onLogIn
+                    openChannel = this@App::openChannel
                 }
             }
         } else {
@@ -70,12 +72,22 @@ class App : RComponent<RProps, AppState>() {
                         setState{
                             friend = state.user.friends.filter { it.second == pickedFriend }.take(1)[0]
                         }
+                        openChannel()
                     }
                 }
                 chat {
                     friendName = state.friend.second
                     messages = state.messages
-                    onSend = this@App::sendMessage
+                    onSend = {
+                        sendMessage(Message(
+                            senderId = state.user.id,
+                            senderName = state.user.name,
+                            recipientId = state.friend.first,
+                            recipientName = state.friend.second,
+                            status = "CHAT",
+                            content = it
+                        ))
+                    }
                 }
             }
         }
@@ -91,9 +103,7 @@ class App : RComponent<RProps, AppState>() {
         if(userList.filter { it.name == username }.isNotEmpty()) {
             val tmpUser = userList.filter { it.name == username }.take(1)[0]
             var socket = js("new SockJS(\"http://localhost:8080/ws\")")
-            console.log(socket)
             val tmpstomp = js("Stomp").over(socket)
-            console.log(tmpstomp)
             tmpstomp.connect(js("{}"), this@App::connectionSuccess, {_-> println("connection failed")})
             setState {
                 user = tmpUser
@@ -106,28 +116,173 @@ class App : RComponent<RProps, AppState>() {
         return true
     }
     private fun connectionSuccess() {
-        println("connected !")
         state.stomp.subscribe("/user/${state.user.id}/queue/messages", this@App::onMessageReceived)
+        openChannel()
     }
-    private fun onMessageReceived(payload: dynamic) {
-        console.log("message received !")
-        console.log(payload)
-    }
-    private fun sendMessage(msg: String) {
-        val message = Message(
-            status = "CHAT",
-            content = msg,
-            senderName = state.user.name,
-            senderId = state.user.id,
-            recipientId = state.friend.first,
-            recipientName = state.friend.second,
-            chatId = "",
-            id = abs(Random.nextInt()).toString())
-        setState {
-            messages = messages + message
+    private fun openChannel() {
+        val asymKeys = js("nacl.box").keyPair()
+        println("private key:")
+        println(asymKeys.secretKey)
+        println("public key:")
+        println(asymKeys.publicKey)
+        val symKey = js("nacl.util.encodeBase64(nacl.randomBytes(nacl.secretbox.keyLength))")
+        println("sym key: $symKey")
+        setState{
+            asymKeyPair = asymKeys
+            symmetricKey = symKey
         }
-        state.stomp.send("/app/chat", js{"{}"},JSON.stringify(message))
+
+//
+//        sendMessage(Message(
+//            content = publicKey,
+//            status = "PUBLICKEY",
+//            senderId = state.user.id,
+//            senderName = state.user.name,
+//            recipientId = state.friend.first,
+//            recipientName = state.friend.second)
+//        )
     }
+
+    private fun onMessageReceived(payload: dynamic) {
+        val message: Message = JSON.parse(payload.body as String)
+        println(message)
+        when(message.status) {
+            "CHAT" -> {
+                setState {
+                    messages = state.messages + decrypt(message, state.symmetricKey)
+                }
+            }
+//            "PUBLICKEY" -> {
+//                setState {
+//                    sharedKey = message.content
+//                }
+//                println("public key send: ")
+//                println(state.asymKeyPair.second)
+//                sendMessage(Message(
+//                    content = state.asymKeyPair.second,
+//                    status = "PUBLICKEYRETURN",
+//                    senderId = state.user.id,
+//                    senderName = state.user.name,
+//                    recipientId = state.friend.first,
+//                    recipientName = state.friend.second)
+//                )
+//            }
+//            "PUBLICKEYRETURN" -> {
+//                println(message.content)
+//                val sharedK = message.content
+//                setState {
+//                    sharedKey = message.content
+//                }
+//                val newNonce = js("nacl.randomBytes(24)")
+//                val messageUint8 = js("nacl.util").decodeBase64(state.symmetricKey)
+//                val encrypted = js("nacl").box(messageUint8, newNonce, js("nacl.util").decodeBase64(sharedK), js("nacl.util").decodeBase64(state.asymKeyPair.first))
+//
+//                var fullMessage = js("new Uint8Array(newNonce.length + encrypted.length)")
+//                fullMessage.set(newNonce)
+//                fullMessage.set(encrypted, newNonce.length)
+//
+//                val base64FullMessage = js("nacl.util").encodeBase64(fullMessage)
+//                sendMessage(Message(
+//                    content = base64FullMessage,
+//                    status = "SECRET",
+//                    nonce = js("nacl.util").encodeBase64(newNonce),
+//                    senderId = state.user.id,
+//                    senderName = state.user.name,
+//                    recipientId = state.friend.first,
+//                    recipientName = state.friend.second)
+//                )
+//            }
+//            "SECRET" -> {
+//                println("secret recived:")
+//                println(message.content)
+//                val messageWithNonceAsUint8Array = js("nacl.util").decodeBase64(message.content)
+//                val tmpNonce = js("messageWithNonceAsUint8Array").slice(0, message.nonce.length)
+//                val msg = js("messageWithNonceAsUint8Array").slice(message.nonce.length, message.content.length)
+//
+//                println("---------------------")
+//                println(state.sharedKey)
+//                println(state.asymKeyPair.second)
+//                val box = js("nacl").box(msg, tmpNonce, state.sharedKey, state.asymKeyPair.second)
+//                val decrypted = js("nacl.box").open(box, tmpNonce, state.sharedKey, state.asymKeyPair.second)
+//
+//                if (!decrypted) {
+//                    println("decryption failed ...")
+//                }
+//
+//                val base64DecryptedMessage = js("nacl.util").encodeUTF8(decrypted)
+//                println(base64DecryptedMessage)
+//                setState {
+////                    symmetricKey = base64DecryptedMessage
+//                }
+//            }
+        }
+    }
+    private fun sendMessage(msg: Message) {
+        if (msg.status == "CHAT") {
+            setState {
+                messages = messages + msg
+            }
+        }
+
+        state.stomp.send("/app/chat", js{"{}"}, JSON.stringify(encrypt(msg, state.symmetricKey)))
+    }
+    fun encrypt(msg: Message, key: dynamic): Message {
+        println("encript method")
+        println("message:  $msg")
+        println(key)
+        val keyUint8Array = js("nacl.util").decodeBase64(key)
+        val newNonce = js("nacl.randomBytes(nacl.secretbox.nonceLength)")
+        val messageUint8 = js("nacl.util").decodeUTF8(msg.content)
+        val box = js("nacl").secretbox(messageUint8, newNonce, keyUint8Array)
+
+        return Message(
+            nonce = cryptoToString(newNonce)?: "crypto error",
+            senderId = msg.senderId,
+            recipientId = msg.recipientId,
+            senderName = msg.senderName,
+            recipientName = msg.recipientName,
+            content = cryptoToString(box)?: "crypto error",
+            status = msg.status)
+    }
+    private fun decrypt(msg: Message, key: dynamic): Message {
+        println("message to decypher: ${msg.content}")
+        val messageText = toCryptho(msg.content)
+        println("raw")
+        println(messageText)
+//        val nonce = messageText.slice(0, js("nacl.secretbox.nonceLength"))
+//        println("nonce")
+//        println(nonce)
+//        val message = messageText.slice(js("nacl.secretbox.nonceLength"),messageText.length)
+
+        println("nonce raw:")
+        println(toCryptho(msg.nonce))
+        val decrypted = js("nacl.secretbox").open(messageText, toCryptho(msg.nonce), js("nacl.util").decodeBase64(key))
+
+        if (!decrypted) {
+            println("Failed to decypher ...")
+        } else {
+//            println(js("nacl.utils").encodeUTF8(decrypted))
+        }
+
+        return Message(nonce = msg.nonce,
+            senderId = msg.senderId,
+            recipientId = msg.recipientId,
+            senderName = msg.senderName,
+            recipientName = msg.recipientName,
+            content = cryptoToString(decrypted) ?: "crypto error",
+            status = msg.status)
+    }
+}
+
+fun toCryptho(string: String) : dynamic {
+
+    val toBase64 = js("btoa")(string)
+    return js("nacl.util").decodeBase64(toBase64)
+}
+
+fun cryptoToString(crypto: dynamic) : String? {
+    val base64 = js("nacl.util").encodeBase64(crypto)
+    return js("atob")(base64)
 }
 
 fun RBuilder.app(handler: RProps.() -> Unit) : ReactElement {
--- client/src/main/kotlin/model/Message.kt
+++ client/src/main/kotlin/model/Message.kt
@@ -1,12 +1,14 @@
 package model
 
+import kotlin.random.Random
+
 data class Message(
-    val id: String,
-    val chatId: String,
+    val id: String = "id" + Random.nextInt(),
+    val nonce: String = "",
     val senderId: String,
     val recipientId: String,
     val senderName: String,
     val recipientName: String,
     val content: String,
-    val status: String
+    val status: String = "CHAT"
 )
